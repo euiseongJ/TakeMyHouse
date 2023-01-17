38320d10b1f0ff6046462cf76fbe9159ffcbc28a
Joonoh <wnsdhe@gmail.com>
2023-01-16 19:15:29 -0500

    Commit adds empty service and controller

--- src/test/java/com/example/demo/TakemyhomeApplicationTests.java
+++ src/test/java/com/example/demo/TakemyhomeApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TakemyhomeApplicationTests {
 
+	@Autowired
+	private MyController myController;
+
+	@Autowired
+	private MyService myService;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(myController).isNotNull();
+		assertThat(myService).isNotNull();
 	}
 
 }
